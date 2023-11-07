43fafceb35c791fba30f7e4e35dac245d4b27f3c
Timbosaurus <tim.j.ericson@gmail.com>
2023-11-07 14:26:42 +1000

    added sum function

--- main.cpp
+++ main.cpp
@@ -4,8 +4,17 @@ using namespace std;
 
 int main()
 {
+    
+    int num1, num2, sum;
+
+    num1 = 10;
+
+    num2 = 20;
+
+    sum = num1 + num2;
+
     cout << "hello world again" << endl;
     //added line
-    
+
     return 0;
 }
